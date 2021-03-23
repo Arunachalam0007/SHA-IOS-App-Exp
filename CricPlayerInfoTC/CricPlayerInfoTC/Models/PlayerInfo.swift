fb40cdcd4c52e9cb246ea34304126b9d8fd85073
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-23 19:17:11 +0530

    Code Refactor: Created MVC Folder Structure

---
+++ CricPlayerInfoTC/CricPlayerInfoTC/Models/PlayerInfo.swift
@@ -0,0 +1,12 @@
+//
+//  PlayerInfo.swift
+//  CricPlayerInfoTC
+//
+//  Created by ArunSha on 23/03/21.
+//
+
+import Foundation
+
+struct PlayerInfo {
+    var playerInfoName : [String] = [ "Raina Four", "Raina Six", "Raina 100", "Raina Catch", "Raina Wicket", "Raina Out"]
+}
