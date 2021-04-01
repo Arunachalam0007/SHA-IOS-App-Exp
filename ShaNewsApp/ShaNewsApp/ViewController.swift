901e837019a38f42a715615217fdb010fb5ff409
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-01 19:17:21 +0530

    Sha_NewsApp: Created Sample code

---
+++ ShaNewsApp/ShaNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShaNewsApp
+//
+//  Created by ArunSha on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
